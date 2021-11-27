6c5fdb10da7eb9874adf7203ed8c31b2565f203b
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2021-11-27 16:42:32 +0100

    Split Projects with Library and Console + Code Architecture Adjustments

---
+++ Nodsoft.WowsReplaysUnpack.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Nodsoft.WowsReplaysUnpack;
+
+Console.WriteLine("Hello, World!");
+
+
+using (FileStream fs = File.OpenRead(Path.Join(Directory.GetCurrentDirectory(), "10.10.wowsreplay")))
+{
+	new ReplayUnpacker().UnpackReplay(fs);
+}
+
+Console.ReadKey();
---
+++ Nodsoft.WowsReplaysUnpack/Data/NetPacket.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+
+namespace Nodsoft.WowsReplaysUnpack.Data;
+
+
+public struct NetPacket
+{
+	public uint Size;
+	public string Type;
+	public float Time;
+	public MemoryStream rawData;
+
+
+	public NetPacket(MemoryStream stream)
+	{
+		byte[] payloadSize = new byte[4];
+		byte[] payloadType = new byte[4];
+		byte[] payloadTime = new byte[4];
+
+		stream.Read(payloadSize);
+		stream.Read(payloadType);
+		stream.Read(payloadTime);
+
+		Size = BitConverter.ToUInt32(payloadSize);
+		Type = BitConverter.ToUInt32(payloadType).ToString("X2");
+		Time = BitConverter.ToSingle(payloadTime);
+
+		byte[] data = new byte[Size];
+		stream.Read(data);
+		rawData = new MemoryStream(data);
+	}
+}
