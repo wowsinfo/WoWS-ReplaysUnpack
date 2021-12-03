9616bc9848104912a225f4ddc1834dcae65eb33d
floribe2000 <43866194+floribe2000@users.noreply.github.com>
2021-12-03 17:47:44 +0100

    Change property assignments to improve performance.

--- Nodsoft.WowsReplaysUnpack/Data/ShipData.cs
+++ Nodsoft.WowsReplaysUnpack/Data/ShipData.cs
@@ -14,47 +14,47 @@ public sealed record ShipData(IReadOnlyList<IReadOnlyList<uint>> ShipConfigurati
 	/// <summary>
 	/// Gets the numeric id of the selected ship.
 	/// </summary>
-	public uint ShipId => ShipConfiguration[Constants.ShipConfigMapping.ShipId].First();
+	public uint ShipId { get; } = ShipConfiguration[Constants.ShipConfigMapping.ShipId].First();
 
 	/// <summary>
 	/// Gets the total number of values that are present in the raw data list.
 	/// Not recommended to use.
 	/// </summary>
-	public uint TotalValueCount => ShipConfiguration[Constants.ShipConfigMapping.TotalValueCount].First();
+	public uint TotalValueCount { get; } = ShipConfiguration[Constants.ShipConfigMapping.TotalValueCount].First();
 
 	/// <summary>
 	/// Gets the list of the ids of the selected ship modules.
 	/// </summary>
-	public IReadOnlyList<uint> ShipModules => ShipConfiguration[Constants.ShipConfigMapping.ShipModules];
+	public IReadOnlyList<uint> ShipModules { get; } = ShipConfiguration[Constants.ShipConfigMapping.ShipModules];
 
 	/// <summary>
 	/// Gets the list of the ids of the selected ship upgrades.
 	/// </summary>
-	public IReadOnlyList<uint> ShipUpgrades => ShipConfiguration[Constants.ShipConfigMapping.ShipUpgrades];
+	public IReadOnlyList<uint> ShipUpgrades { get; } = ShipConfiguration[Constants.ShipConfigMapping.ShipUpgrades];
 
 	/// <summary>
 	/// Gets the list of the selected exterior components.
 	/// Usually signals, but it can contain other exterior stuff as well.
 	/// </summary>
-	public IReadOnlyList<uint> ExteriorSlots => ShipConfiguration[Constants.ShipConfigMapping.ExteriorSlots];
+	public IReadOnlyList<uint> ExteriorSlots { get; } = ShipConfiguration[Constants.ShipConfigMapping.ExteriorSlots];
 
 	/// <summary>
 	/// Gets the auto supply state. It's unclear how this is calculated.
 	/// </summary>
-	public uint AutoSupplyState => ShipConfiguration[Constants.ShipConfigMapping.AutoSupplyState].First();
+	public uint AutoSupplyState { get; } = ShipConfiguration[Constants.ShipConfigMapping.AutoSupplyState].First();
 
 	/// <summary>
 	/// Gets the list of color scheme data.
 	/// </summary>
-	public IReadOnlyList<uint> ColorScheme => ShipConfiguration[Constants.ShipConfigMapping.ColorScheme];
+	public IReadOnlyList<uint> ColorScheme { get; } = ShipConfiguration[Constants.ShipConfigMapping.ColorScheme];
 
 	/// <summary>
 	/// Gets the list of the selected consumables.
 	/// </summary>
-	public IReadOnlyList<uint> ConsumableSlots => ShipConfiguration[Constants.ShipConfigMapping.ConsumableSlots];
+	public IReadOnlyList<uint> ConsumableSlots { get; } = ShipConfiguration[Constants.ShipConfigMapping.ConsumableSlots];
 
 	/// <summary>
 	/// Gets the currently mounted flags of the ship.
 	/// </summary>
-	public IReadOnlyList<uint> Flags => ShipConfiguration[Constants.ShipConfigMapping.Flags];
+	public IReadOnlyList<uint> Flags { get; } = ShipConfiguration[Constants.ShipConfigMapping.Flags];
 }
