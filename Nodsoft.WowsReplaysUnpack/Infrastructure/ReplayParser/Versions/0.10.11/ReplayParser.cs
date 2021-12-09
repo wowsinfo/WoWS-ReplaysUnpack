621e33b6a9256e215e6c28b986760477ebd2ce57
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2021-12-09 17:38:36 +0100

    Implement 0.10.11 replay parser

---
+++ Nodsoft.WowsReplaysUnpack/Infrastructure/ReplayParser/Versions/0.10.11/ReplayParser.cs
@@ -0,0 +1,19 @@
+﻿using Nodsoft.WowsReplaysUnpack.Data;
+using System.Collections.Generic;
+using System.Collections;
+using System.Reflection;
+using System;
+using System.Linq;
+using Mapster;
+using Nodsoft.WowsReplaysUnpack.Data.Raw;
+using Razorvine.Pickle;
+using System.IO.Compression;
+using System.IO;
+using System.Text;
+
+namespace Nodsoft.WowsReplaysUnpack.Infrastructure.ReplayParser.Versions;
+
+internal class ReplayParser_0_10_11 : ReplayParser_0_10_10
+{
+	public override IReplayMessageTypes MessageTypes { get; } = new Constants_0_10_11.ReplayMessageTypes();
+}
