0862e5dee7d54974cfaf6491d9737af977b6df10
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2022-09-12 07:44:15 +0200

    HACK: Disable tests parallelization
    This commit mitigates issue #23, pending a more potent fix.

--- Nodsoft.WowsReplaysUnpack.Tests/ReplayParsingExecutionTests.cs
+++ Nodsoft.WowsReplaysUnpack.Tests/ReplayParsingExecutionTests.cs
@@ -4,6 +4,11 @@ using Nodsoft.WowsReplaysUnpack.FileStore.Definitions;
 using Nodsoft.WowsReplaysUnpack.Services;
 using Xunit;
 
+
+/*
+ * FIXME: Test parallelization is disabled due to a file loading issue.
+ */
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
 namespace Nodsoft.WowsReplaysUnpack.Tests;
 
 /// <summary>
