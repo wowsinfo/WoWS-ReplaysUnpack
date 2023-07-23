84418aee0f306ab7d61626d0c37e49f803789235
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2023-07-23 03:03:58 +0200

    feat(tests): Add ReplayUnpackerTests and sample replay file for 12.6.0
    This commit adds the `ReplayUnpackerTests` class to the test project, which includes a test method `TestReplay_Pass` for parsing a working replay file. The replay file used in the test is "12.6.wowsreplay", which has been added to the "Replay-Samples" folder.
    
    The `ReplayUnpackerTests` class sets up the necessary dependencies using dependency injection and tests that the unpacked replay object is not null after unpacking.

---
+++ Nodsoft.WowsReplaysUnpack.Tests/ReplayUnpackerTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Nodsoft.WowsReplaysUnpack.Core.Exceptions;
+using Nodsoft.WowsReplaysUnpack.Core.Models;
+using Nodsoft.WowsReplaysUnpack.Services;
+using Xunit;
+
+namespace Nodsoft.WowsReplaysUnpack.Tests;
+
+public class ReplayUnpackerTests
+{
+	private readonly ReplayUnpackerFactory _factory;
+	private readonly string _sampleFolder = Path.Join(Directory.GetCurrentDirectory(), "../../../..", "Replay-Samples");
+
+	public ReplayUnpackerTests()
+	{
+		_factory = new ServiceCollection()
+			.AddLogging(l => l.ClearProviders())
+			.AddWowsReplayUnpacker()
+			.BuildServiceProvider()
+			.GetRequiredService<ReplayUnpackerFactory>();
+	}
+
+	
+	/// <summary>
+	/// Test parsing a working replay (Assets/good.wowsreplay)
+	/// </summary>
+	[
+		Theory,
+		InlineData("0.10.10.wowsreplay"),
+		InlineData("0.10.11.wowsreplay"),
+		InlineData("0.11.0.wowsreplay"),
+		InlineData("0.11.2.wowsreplay"),
+		InlineData("12.6.wowsreplay"),
+	]
+	public void TestReplay_Pass(string replayPath)
+	{
+		UnpackedReplay replay = _factory.GetUnpacker().Unpack(LoadReplay(replayPath));
+		Assert.NotNull(replay);
+	}
+	
+	
+	private MemoryStream LoadReplay(string replayPath)
+	{
+		using FileStream fs = File.OpenRead(Path.Join(_sampleFolder, replayPath));
+		MemoryStream ms = new();
+		fs.CopyTo(ms);
+		ms.Position = 0;
+
+		return ms;
+	}
+}
